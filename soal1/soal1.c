ad7606b1ca4c5c020a136dfc62792a1f88f39a48
karinasraya <37465709+karinasraya@users.noreply.github.com>
2019-04-10 16:29:29 +0700

    FIX

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -5,35 +5,39 @@
 #include<unistd.h>
 
 void* hitung(void *args){
-	int b=strtol((char*)args,NULL,10);
+	int *b=(int *)args;
 	int hasil=1;
 	if(b<0){
 		printf("ERROR!\n");
 	}
 	else{
-	for(int i=1;i<=b;i++){
+	for(int i=1;i<=*b;i++){
 		hasil*=i;
 	}
-	printf("%d! = %d\n",b,hasil);
+	printf("%d! = %d\n",*b,hasil);
 	}
 }
 
 int main(int argc, char *args[]){
-	char* temp;
 	pthread_t tid[argc-1];
+	int konfersi[100];
 
-	for(int i=1;i<argc-1;i++){
-		for(int j=0;j<(argc-i-1);j++){
-		if(strcmp(args[j+1],args[j+2])<0){
-			temp=args[j+1];
-			args[j+1]=args[j+2];
-			args[j+2]=temp;
+	for(int i=0;i<argc-1;i++){
+		konfersi[i]=atoi(args[i+1]);
+	}
+
+	for(int i=0;i<argc-2;i++){
+		for(int j=0;j<(argc-i-2);j++){
+		if(konfersi[j]<konfersi[j+1]){
+			int temp=konfersi[j];
+			konfersi[j]=konfersi[j+1];
+			konfersi[j+1]=temp;
 		}
 		}
 	}
 
 	for(int i=0;i<argc-1;i++){
-		pthread_create(&(tid[i]),NULL,&hitung,(void*)args[i+1]);
+		pthread_create(&(tid[i]),NULL,&hitung,(void*)&konfersi[i]);
 	}
 
 	for(int i=0;i<argc-1;i++){
